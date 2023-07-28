09148d9e1fd6ecab6418729349ee18cd4148c3b6
shashyabh <shashyabhray@gmail.com>
2023-07-29 00:35:01 +0530

    Message

--- src/Config/Api.js
+++ src/Config/Api.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = `http://localhost:2525`;
-export const BASE_URL = `https://whatsapp-backend-production-4929.up.railway.app`;
+export const BASE_URL = `http://whatsapp-backend-production-4929.up.railway.app`;
